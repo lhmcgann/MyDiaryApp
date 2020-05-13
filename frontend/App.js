7d5927d310f2f49271e70af5b65fe9f9a756fe5d
lhmcgann <hailey.mcgann@gmail.com>
2020-05-13 12:40:27 -0700

    added link to readme, formatted App.js and DiaryList.js to test prettier

--- frontend/App.js
+++ frontend/App.js
@@ -1,56 +1,59 @@
-import React, { Component } from 'react'
-import DiaryList from './DiaryList'
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import DiaryList from "./DiaryList";
+import axios from "axios";
+import { Link } from "react-router-dom";
 class App extends Component {
   state = {
-    characters : [],
-  }
+    characters: [],
+  };
   componentDidMount() {
-    axios.get(`http://localhost:5000/users/`)
-    .then(res => {
-      const characters = res.data.users_list;
-      this.setState({characters});
-    })
-    .catch(function (error) {
-      //Not handling the error. Just logging into the console.
-      console.log(error);
-    });
+    axios
+      .get(`http://localhost:5000/users/`)
+      .then((res) => {
+        const characters = res.data.users_list;
+        this.setState({ characters });
+      })
+      .catch(function (error) {
+        //Not handling the error. Just logging into the console.
+        console.log(error);
+      });
   }
-  makePostCall(character){
-   return axios.post('http://localhost:5000/users', character)
-    .then(function (response) {
-      console.log(response);
-      response.status = 201;
-      return response.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-      return false;
-    });
- }
- makeDeleteCall(character){
-  return axios.delete('http://localhost:5000/users/'+character.id)
-  .then(function (response) {
-    console.log(response);
-    return true;
-  })
-  .catch(function (error) {
-    console.log(error);
-    return false;
-  });
+  makePostCall(character) {
+    return axios
+      .post("http://localhost:5000/users", character)
+      .then(function (response) {
+        console.log(response);
+        response.status = 201;
+        return response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return false;
+      });
+  }
+  makeDeleteCall(character) {
+    return axios
+      .delete("http://localhost:5000/users/" + character.id)
+      .then(function (response) {
+        console.log(response);
+        return true;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return false;
+      });
+  }
+  render() {
+    const { characters } = this.state;
+    return (
+      <div class="centered">
+        <h1>Welcome to MyDiary</h1>
+        <DiaryList characterData={characters} />
+        <Link to="/new-diary/">
+          <button class="button1">+ Add Diary</button>
+        </Link>
+      </div>
+    );
+  }
 }
-    render() {
-      const { characters } = this.state
-      return (
-       <div class="centered">
-         <h1>Welcome to MyDiary</h1>
-            <DiaryList characterData={characters}/>
-            <Link to="/new-diary/">
-                <button class="button1">+ Add Diary</button>
-            </Link>
-       </div>
-     );
-   }
-}
-export default App
+export default App;
