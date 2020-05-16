d16dd7d0f536a45bbe55cb2bb35d3519bf2d5cad
lhmcgann <hailey.mcgann@gmail.com>
2020-05-16 13:35:58 -0700

    spearated db and other backend code; added cluster uri and correct db access names

--- backend/backend.py
+++ backend/backend.py
@@ -2,75 +2,21 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
-from pymongo import MongoClient
-from bson import ObjectId
-import pymongo
 import ssl
-
 
 
 app = Flask(__name__)
 CORS(app)
 
 
-
-class Model(dict):
-    """
-    A simple model that wraps mongodb document
-    """
-    __getattr__ = dict.get
-    __delattr__ = dict.__delitem__
-    __setattr__ = dict.__setitem__
-
-    def save(self):
-        if not self._id:
-            self.collection.insert(self)
-        else:
-            self.collection.update(
-                { "_id": ObjectId(self._id) }, self)
-        self._id = str(self._id)
-
-    def reload(self):
-        if self._id:
-            result = self.collection.find_one({"_id": ObjectId(self._id)})
-            if result :
-                self.update(result)
-                self._id = str(self._id)
-                return True
-        return False
-
-    def remove(self):
-        if self._id:
-            resp = self.collection.remove({"_id": ObjectId(self._id)})
-            self.clear()
-            return resp
-
-
-class User(Model):
-    cluster = pymongo.MongoClient('localhost', 27017)
-    db = cluster["diary"]
-    collection = db["diary"]
-    def find_all(self):
-        users = list(self.collection.find())
-        for user in users:
-              user["_id"] = str(user["_id"])
-        return users
-
-    def find_by_name(self, name):
-        users = list(self.collection.find({"name": name}))
-        for user in users:
-              user["_id"] = str(user["_id"])
-        return users
-
-
-@app.route('/users', methods=['GET', 'POST'])
-def get_users():
+@app.route('/entries', methods=['GET', 'POST'])
+def get_entries():
     if request.method == "GET":
-        diaries = User().find_all()
-        return {"diaries": diaries}, 200
+        entries = Entry().find_all()
+        return {"entries": entries}, 200
     if request.method == 'POST':
-        userToAdd = request.get_json()
-        newUser = User(userToAdd)
-        newUser.save()
-        resp = jsonify(newUser), 200
+        entryToAdd = request.get_json()
+        newEntry = Entry(entryToAdd)
+        newEntry.save()
+        resp = jsonify(newEntry), 200
         return resp
---
+++ backend/model_mongodb.py
@@ -0,0 +1,54 @@
+import pymongo
+from pymongo import MongoClient
+from bson import ObjectId
+
+
+class Model(dict):
+    """
+    A simple model that wraps mongodb document
+    """
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                {"_id": ObjectId(self._id)}, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result:
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.remove({"_id": ObjectId(self._id)})
+            self.clear()
+            return resp
+
+
+class Entry(Model):
+    uri = 'mongodb+srv://client:mydiaryapp@cluster0-k792t.azure.mongodb.net/test?retryWrites=true&w=majority'
+    cluster = pymongo.MongoClient(uri)
+    db = cluster["diary1"]
+    collection = db["diary1"]
+
+    def find_all(self):
+        entries = list(self.collection.find())
+        for entry in entries:
+            entry["_id"] = str(entry["_id"])
+        return entries
+
+    def find_by_title(self, title):
+        entries = list(self.collection.find({"title": title}))
+        for entry in entries:
+            entry["_id"] = str(entry["_id"])
+        return entries
