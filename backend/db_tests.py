b90b7398f269717a6395fc2c8f1c5b2c5dcdef60
lhmcgann <hailey.mcgann@gmail.com>
2020-05-20 15:59:07 -0700

    changed db pytests, updated requirements.txt; idk what old warning from

--- backend/db_tests.py
+++ backend/db_tests.py
@@ -4,13 +4,21 @@ from pymongo import MongoClient
 from model_mongodb import *
 
 
-uri = 'mongodb+srv://client:mydiaryapp@cluster0-k792t.azure.mongodb.net/test?re\
-    tryWrites=true&w=majority'
-db = pymongo.MongoClient(uri)["myDiaryApp"]
-d_collection = db["diaries"]
+# uri = 'mongodb+srv://client:mydiaryapp@cluster0-k792t.azure.mongodb.net/test?re\
+#     tryWrites=true&w=majority'
+# db = pymongo.MongoClient(uri)["myDiaryApp"]
+# d_collection = db["diaries"]
+
+# def test_find_all_diaries():
+#     num_diaries = len(d_collection.find())
+#     diaries = Diary().find_all()
+#     assert num_diaries == len(diaries)
+
+def test_diary_reload_DNE():
+    diary = Diary()
+    assert diary.reload() is False
 
 
-def test_find_all_diaries():
-    num_diaries = len(d_collection.find())
-    diaries = Diary().find_all()
-    assert num_diaries == len(diaries)
+def test_diary_reload_bad_id():
+    diary = Diary({"_id": "bad id"})
+    assert diary.reload() is False
