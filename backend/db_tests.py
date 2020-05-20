ded4700268429fed56b654bc5c0b68b8e0793788
lhmcgann <hailey.mcgann@gmail.com>
2020-05-20 15:40:04 -0700

    pytest (one warning)

---
+++ backend/db_tests.py
@@ -0,0 +1,16 @@
+import pytest
+import pymongo
+from pymongo import MongoClient
+from model_mongodb import *
+
+
+uri = 'mongodb+srv://client:mydiaryapp@cluster0-k792t.azure.mongodb.net/test?re\
+    tryWrites=true&w=majority'
+db = pymongo.MongoClient(uri)["myDiaryApp"]
+d_collection = db["diaries"]
+
+
+def test_find_all_diaries():
+    num_diaries = len(d_collection.find())
+    diaries = Diary().find_all()
+    assert num_diaries == len(diaries)
