da86b58c8cfdfed421ca99f52a31d381960f20a2
sarbor <sarboroy99@gmail.com>
2020-05-21 22:53:51 -0700

    doing sample backend test

--- backend/backend_tests.py
+++ backend/backend_tests.py
@@ -5,4 +5,6 @@ from backend import *
 
 def test_get_diaries_get():
     "GET request to url returns a 200"
-    url = 'https://localhost:3000/diaries'
+    url = 'https://localhost:5000/diaries'
+    req = requests.get(url)
+    print(req)
