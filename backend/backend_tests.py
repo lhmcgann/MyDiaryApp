aed17f7cc6cefa5e78bd365cf34617126dd5385d
lhmcgann <hailey.mcgann@gmail.com>
2020-05-24 17:56:14 -0700

    does thi help build at all

--- backend/backend_tests.py
+++ backend/backend_tests.py
@@ -1,7 +1,8 @@
 import pytest
-import requests
+# import requests
+
 
 def test_get_diaries_get():
-	print("hello")
-	url = 'http://127.0.0.1:5000/diaries'
-	req = requests.get(url)
+    print("hello")
+    url = 'http://127.0.0.1:5000/diaries'
+    # req = requests.get(url)
