62f9e1042e648847dc09b0d8cafa3ed2235bea44
lhmcgann <hailey.mcgann@gmail.com>
2020-05-27 13:21:37 -0700

    style checker fixes; deleted merge comments in yml

--- backend/backend_tests.py
+++ backend/backend_tests.py
@@ -1,9 +1,8 @@
 import pytest
-# import requests
+import requests
 
 
 def test_get_diaries_get():
-	url = 'http://127.0.0.1:5000/diaries'
-	req = requests.get(url)
-	print(req.status_code)
-
+    url = 'http://127.0.0.1:5000/diaries'
+    req = requests.get(url)
+    print(req.status_code)
