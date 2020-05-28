a34c3997aaac62a24f5ebe9f5cea4135f2760c0a
lhmcgann <hailey.mcgann@gmail.com>
2020-05-28 11:37:34 -0700

    just reorganized tests

--- backend/db_tests.py
+++ backend/db_tests.py
@@ -12,6 +12,22 @@ def test_in_testing_mode():
     assert TEST is True
 
 
+def test_entry_gets_TEST():
+    entry = Entry()
+    clxns = entry.db.list_collection_names()
+    expect = ["tests", "diaries", "entries"]
+    for item in clxns:
+        assert item in expect
+
+
+def test_diary_gets_TEST():
+    diary = Diary()
+    clxns = diary.db.list_collection_names()
+    expect = ["tests", "diaries", "entries"]
+    for item in clxns:
+        assert item in expect
+
+
 def test_diary_save_new():
     doc = {"title": "test_diary_save_new", "dateCreated": "TODO: need Date()",
            "entries": []}
@@ -57,8 +73,17 @@ def test_diary_remove():
     assert isinstance(id, str)
 
     diary = Diary({"_id": id})
+    assert (diary == {}) is False
     diary.remove()
     assert (diary == {}) is True
+
+
+def test_diary_reload_dne():
+    diary = Diary()
+    assert diary.reload() is False
+
+    d2 = Diary({"random field": "random value"})
+    assert diary.reload() is False
 
 
 def test_diary_reload():
@@ -69,22 +94,6 @@ def test_diary_reload():
     assert isinstance(diary["_id"], str) is True
     for item in diary_model:
         assert item in diary
-
-
-def test_entry_gets_TEST():
-    entry = Entry()
-    clxns = entry.db.list_collection_names()
-    expect = ["tests", "diaries", "entries"]
-    for item in clxns:
-        assert item in expect
-
-
-def test_diary_gets_TEST():
-    diary = Diary()
-    clxns = diary.db.list_collection_names()
-    expect = ["tests", "diaries", "entries"]
-    for item in clxns:
-        assert item in expect
 
 
 def test_new_entry_no_diary():
@@ -113,11 +122,6 @@ def test_new_entry_with_diary():
     diary = Diary({"_id": d_id})
     diary.reload()
     assert res["_id"] in diary["entries"]
-
-
-def test_diary_reload_dne():
-    diary = Diary()
-    assert diary.reload() is False
 
 
 # uri = 'mongodb+srv://client:mydiaryapp@cluster0-k792t.azure.mongodb.net/test?re\
