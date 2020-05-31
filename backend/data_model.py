27ec78b4bfd696c4662eee8206d810c207ca62f9
sarbor <sarboroy99@gmail.com>
2020-05-31 00:44:21 -0700

    Finished implementing a bunch more endpoints using the server memory and added default values for the server diaries to look like a database

--- backend/data_model.py
+++ backend/data_model.py
@@ -1,12 +1,12 @@
 import time
 import json
-
+import datetime
 
 class Diary:
-    def __init__(self, title):
+    def __init__(self, title=""):
         self.title = title
         self.entries = []
-        self.dateCreated = time.time()
+        self.dateCreated = datetime.datetime.now()
         self.id = id(self)
 
     def sortEntriesMostRecent(self):
@@ -19,14 +19,36 @@ class Diary:
         newEntry = Entry(title)
         self.entries.append(newEntry)
 
+    def appendEntry(self, entry):
+        self.entries.append(entry)
+
+    def json(self):
+        jsonObject = self.__dict__
+        jsonObject["entries"] = [entry.__dict__ for entry in jsonObject["entries"]]
+        return jsonObject
+
+    def removeEntry(self, entryId):
+        for i, entry in enumerate(self.entries):
+            if entry.id == entryId:
+                del self.entries[i]
+                break
+
+
 
 class Entry:
-    def __init__(self, title):
+    def __init__(self, title=""):
         self.title = title
         self.tags = []
         self.text = ""
-        self.date = time.time()
+        self.date = datetime.datetime.now()
         self.id = id(self)
+
+    def updateEntry(self, title, tags, text):
+        self.title = title
+        self.text = text
+
+        for tag in tags:
+            self.add_tag(tag)
 
     def update_title(self, title):
         self.title = title
@@ -41,3 +63,6 @@ class Entry:
     def remove_tag(self, tag):
         if tag in self.tags:
             self.tags.remove(tag)
+
+    def json(self):
+        return self.__dict__
