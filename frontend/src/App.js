9a6ae597dfee7125f1277866edaf5157931afb9f
twrappe <twrappe@gmail.com>
2020-06-08 10:14:48 -0700

    Style for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,27 @@ import React, { Component } from "react";
 import Home from "./components/Home";
 import DiaryHome from "./components/DiaryHome";
 import ViewEntry from "./components/ViewEntry";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles.css";
 class App extends Component {
   render() {
-      return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/diaries/:d_id/entries/:e_id" render={(props) => <ViewEntry {...props}/>}/>
-        <Route path="/diaries/:d_id" render={(props) => <DiaryHome {...props}/>}/>
-      </Switch>
-    </Router>
-    )
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route
+            path="/diaries/:d_id/entries/:e_id"
+            render={(props) => <ViewEntry {...props} />}
+          />
+          <Route
+            path="/diaries/:d_id"
+            render={(props) => <DiaryHome {...props} />}
+          />
+        </Switch>
+      </Router>
+    );
   }
 }
 export default App;
